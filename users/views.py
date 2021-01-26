67441301ff0f68af21bf062216a73bcf2c1f2ca5
junhee Kim <junhee1469@gmail.com>
2021-01-26 20:10:35 +0900

    poss

--- users/views.py
+++ users/views.py
@@ -8,7 +8,8 @@ from django.contrib.auth import authenticate, login, logout
 from bs4 import BeautifulSoup
 
 # Create your views here.
-
+cbox = []
+lotto = []
 '''
     if form.is_valid():
         return render(request,"lottery.html",context={
@@ -22,6 +23,45 @@ from bs4 import BeautifulSoup
         })
     '''
 class InputLottery(View):
+
+    def l_num(self, page_num):
+        global lotto
+        global cbox
+        try:
+            url = requests.get(f"https://kr.lottolyzer.com/history/south-korea/6_slash_45-lotto/page/{page_num}/per-page/50/summary-view")
+        except Exception:
+            return 0
+        indeed_soup = BeautifulSoup(url.text,"html.parser")
+        last_num_table = indeed_soup.find("tbody").findAll("tr")
+        first_num = last_num_table[0].find("td").text
+        if first_num == "기록이 없습니다":
+            return 0
+        last_num = last_num_table[len(last_num_table)-1].find("td").text
+        for i in range(int(last_num), int(first_num)+1):
+            cbox.append(list(map(lambda x:int(x.strip()),last_num_table[i-int(last_num)].findAll("td",{"class":"sum-p1"})[1].text.split(','))))
+    
+        return int(last_num)
+
+    def exe(self):
+        global lotto
+        global cbox
+        if len(cbox) and len(lotto):
+            return lotto
+        else:
+            page = 1
+            while self.l_num(page):
+                page+=1
+            print(cbox)
+
+            lotto = [0]*46
+            size = len(cbox)
+            for num in range(1,46):
+                c = 0
+                for i in range(size):
+                    if num in cbox[i]:
+                        c+=1
+                lotto[num] = (c/size)
+            return lotto
 
     def post(self, request):
 
@@ -45,7 +85,26 @@ class InputLottery(View):
                     c+=1
                     if form.cleaned_data.get(f"num{i}") == list(container+bonus)[6]:
                         isbonus = True
-            return render(request,"galleries/gallery_list.html", context={'form':form, "container":container, "bonus":bonus,"done":True, "count":c,"eight":8, "isbonus":isbonus})
+            self.exe()
+            num1 = int(form.cleaned_data.get(f"num1"))
+            num2 = int(form.cleaned_data.get(f"num2"))
+            num3 = int(form.cleaned_data.get(f"num3"))
+            num4 = int(form.cleaned_data.get(f"num4"))
+            num5 = int(form.cleaned_data.get(f"num5"))
+            num6 = int(form.cleaned_data.get(f"num6"))
+            pro = lotto[num1]*lotto[num2]*lotto[num3]*lotto[num4]*lotto[num5]*lotto[num6]
+            expected = 1000*(int(1000*(1/pro))//1000)
+            return render(request,"galleries/gallery_list.html", context={
+                'form':form, 
+                "container":container, 
+                "bonus":bonus,
+                "done":True, 
+                "count":c,
+                "eight":8, 
+                "isbonus":isbonus,
+                "lotto":lotto,
+                "expected":expected,
+            })
 
         return render(request,"galleries/gallery_list.html", context={'form':form, "done":None})
     def get(self, request):
